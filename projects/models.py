ebf9f9cdff967405c9833ed1aeb07929e0995b7e
Njoro90260 <wchegesalome@gmail.com>
2025-01-23 23:58:24 +0300

    projects app models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    """The projects that will be managed model."""
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deadline = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class TeamMember(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='team_menbers')
+    role = models.CharField(max_length=100, choices=[('Manager', 'Manager'), ('Developer', 'Developer'), ('Tester', 'Tester')])
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role}) - {self.project.name}"
+    
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='tasks')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=50,
+        choices=[
+            ('To Do', 'To Do'),
+            ('In Progress', 'In Progress'),
+            ('Completed', 'Completed'),
+        ],
+        default='To Do'
+    )
+    priority = models.CharField(
+        max_length=20,
+        choices=[
+            ('Low', 'Low'),
+            ('Medium', 'Medium'),
+            ('High', 'High'),
+        ],
+        default='Medium'
+    )
+    start_date = models.DateField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.project.name}"
+    
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.task.title}"
