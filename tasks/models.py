5c5f9d25118d83da70348627baff87cbdff20590
Njoro90260 <wchegesalome@gmail.com>
2025-01-24 17:59:58 +0300

    loaded fixtures, made tasks migrations.

--- tasks/models.py
+++ tasks/models.py
@@ -41,3 +41,20 @@ class TaskAttachment(models.Model):
 
     def __str__(self):
         return f"Attachment for {self.task.title} - {self.file.name}"
+    
+class TaskHistory(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='history')
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    change_description = models.TextField()
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History of {self.task.title} - Updated by {self.updated_by.username if self.updated_by else 'Unknown'}"
+    
+class TaskChecklists(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='checklists')
+    item_name = models.CharField(max_length=255)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.item_name} ({'Completed' if self.is_completed else 'Pending'}) - {self.task.title}"
