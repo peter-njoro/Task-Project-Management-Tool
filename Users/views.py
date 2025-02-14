a8a6bc3b1aee722b8451907ea9e4b84eeded765c
Peter <wchegesalome@gmail.com>
2025-02-14 18:28:49 +0300

    user default team member.

--- Users/views.py
+++ Users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import CustomUserCreationForm
+from .models import Role
+from projects.models import ProjectRole
 
 def register(request):
     """Register a new user."""
@@ -14,6 +16,9 @@ def register(request):
 
         if form.is_valid():
             new_user = form.save()
+            # Assign a default role (e.g., "Team Member") to the user
+            default_role, created = Role.objects.get_or_create(name='Team Member')
+            new_user.roles.add(default_role)
             # Log the user and then redirect to the Dashboard.
             login(request, new_user)
             messages.success(request, "User registered and logged in successfully.")
