d02a4c69b8b84d91cc31a60713b24c906a49ca35
Peter Njoroge <wchegesalome@gmail.com>
2025-02-24 14:33:16 +0300

    file tags.

--- tasks/views.py
+++ tasks/views.py
@@ -69,11 +69,12 @@ def kanban_board(request):
 def task_detail(request, task_id):
     """Displays the task details along with its comments."""
     task = get_object_or_404(Task, id=task_id)
+    files = task.files.all() 
     comments = Comment.objects.filter(task=task).order_by('-created_at')
     context = {
         "task": task,
-        "comments": comments
-        
+        "comments": comments,
+        "files": files
     }
     return render(request, "tasks/task_detail.html", context)
 
