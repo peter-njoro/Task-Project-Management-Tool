65ee2b45982fcb7d1577942225aaad37a110d9ae
Peter <wchegesalome@gmail.com>
2025-03-27 07:34:56 +0300

    deleted unused javascript files

--- static/js/globalScripts.js
+++
@@ -1,9 +0,0 @@
-// globalScripts.js - Load globally required scripts
-import { loadScript } from "./scriptLoader.js";
-
-export function loadGlobalScripts() {
-    loadScript("/static/js/search.js");
-    loadScript("/static/js/notifications.js");
-    loadScript("/static/js/theme.js");
-}
-
--- static/js/scriptLoader.js
+++
@@ -1,9 +0,0 @@
-// scriptLoader.js - Utility to load scripts dynamically
-export function loadScript(src) {
-    if (!document.querySelector(`script[src='${src}']`)) { // Prevent duplicate loading
-        const script = document.createElement("script");
-        script.src = `${src}?v=${new Date().getTime()}`; // Prevent caching issues
-        script.async = true;
-        document.head.appendChild(script);
-    }
-}
