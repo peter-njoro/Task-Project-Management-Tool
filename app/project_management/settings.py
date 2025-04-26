6e1cb52465757685f7a70ffc3d5c7a4dc1502136
Peter Njoroge <wchegesalome@gmail.com>
2025-04-26 11:39:44 +0300

    minor layout changes.

--- app/project_management/settings.py
+++ app/project_management/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.getenv('DEBUG', 0)))
+DEBUG = bool(int(os.getenv('DEBUG')))
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.getenv('ALLOWED_HOSTS')
