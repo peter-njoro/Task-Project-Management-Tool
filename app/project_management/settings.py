a6e5cb6eec891dc5415514b7693d7f21c0791ce1
Njoro90260 <wchegesalome@gmail.com>
2025-05-06 22:07:03 +0300

    load env absolutely.

--- app/project_management/settings.py
+++ app/project_management/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import socket
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(dotenv_path=os.path.join(BASE_DIR.parent, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
